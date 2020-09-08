503dd6d193a33f6a41599665646bbfae9a3c0333
Krauszzsolt <36264501+Krauszzsolt@users.noreply.github.com>
2020-09-08 11:31:56 +0200

    Add component and services frame

--- Frontend/EasyEOrder/src/app/feature/food/food.module.ts
+++ Frontend/EasyEOrder/src/app/feature/food/food.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FoodRoutingModule } from './food-routing.module';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodAddComponent } from './food-add/food-add.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FoodListComponent, FoodAddComponent],
   imports: [
     CommonModule,
     FoodRoutingModule
